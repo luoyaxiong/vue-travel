79705e0cea771ea6d766452b7eaa35583785bf16
Luo <luoyaxiong159@gmail.com>
2020-11-22 18:22:25 +0100

    finish list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,6 +2,7 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
+      <city-list :hotCities="hotCities" :cities="cities"></city-list>
     </div>
 
 </template>
@@ -9,11 +10,39 @@
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import axios from 'axios'
+
 export default {
   name: 'City',
+  data () {
+    return {
+      hotCities: [],
+      cities: {}
+    }
+  },
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
+  },
+  methods: {
+    getCityInfo () {
+      // 这里then传入的是函数名，作为argument 不是调用
+      axios.get('/api/city.json').then(this.getCitySucc)
+    },
+    getCitySucc (res) {
+      console.log(res)
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 
 }
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="list" ref='wrapper'>
+    <div>
+        <div class="area">
+                    <div class="title border-topbottom">当前城市</div>
+                    <div class="button-list">
+                        <div class="button-wrapper">
+                            <div class="button">北京</div>
+                        </div>
+                    </div>
+        </div>
+
+        <div class="area">
+                    <div class="title border-topbottom">热门城市</div>
+                    <div class="button-list">
+                        <div class="button-wrapper"
+                        v-for="item of hotCities"
+                        :key="item.id">
+                            <div class="button">{{item.name}}</div>
+                        </div>
+                    </div>
+        </div>
+        <div  v-if="citiesLen">
+            <div class="area"
+                v-for="(item, key) of cities"
+                :key="key"
+                :ref="key">
+                    <div class="title border-topbottom">{{key}}</div>
+                    <div class="item-list ">
+                        <div class="item border-bottom"
+                        v-for="innerItem of item"
+                        :key="innerItem.id">
+                                {{innerItem.name}}
+                        </div>
+                    </div>
+
+        </div>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+
+export default {
+  name: 'CityList',
+  props: {
+    hotCities: Array,
+    cities: Object
+  },
+  computed: {
+    citiesLen () {
+      return Object.keys(this.cities).length
+    }
+  },
+  mounted () {
+    // this.$nextTick 是一个异步函数，为了确保 DOM 已经渲染,底层用到了 MutationObserver 或者是 setTimeout(fn, 0)。其实我们在这里把 this.$nextTick 替换成 setTimeout(fn, 20) 也是可以的（20 ms 是一个经验值，每一个 Tick 约为 17 ms），对用户体验而言都是无感知的。
+    // 这里还有个小bug 就是刚加载进入不能滑动: 加长延时的时间就可以解决
+    setTimeout(() => {
+      this.scroll = new BScroll(this.$refs.wrapper)
+    }, 60)
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+    .border-topbottom
+     &:before
+        border-color  #ccc
+     &:after
+        border-color  #ccc
+    .border-bottom
+     &:before
+        border-color  #ccc
+    .list
+        overflow hidden
+        position absolute
+        top 1.58rem
+        left 0
+        right 0
+        bottom 0
+
+        .title
+            line-height .54rem
+            background  #eee
+            padding-left  .2rem
+            color #666
+            font-size .26rem
+        .button-list
+            overflow hidden
+            padding .1rem .6rem .1rem .1rem
+            padding .1ren
+            .button-wrapper
+                float left
+                width 33.33%
+                .button
+                    margin .1rem
+                    padding .1rem 0
+                    text-align center
+                    border  .02rem solid #ccc
+                    border-radius .06rem
+        .item-list
+            line-height  .58rem
+            color #666
+            padding-left .2rem
+
+</style>
