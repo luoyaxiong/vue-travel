57849571ffd77e8d952ffe0d8251b5f63d49a23a
Luo <luoyaxiong159@gmail.com>
2020-11-24 10:13:00 +0100

    updata ajax params

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -30,7 +30,11 @@ export default {
   },
   methods: {
     getDetailInfo () {
-      axios.get('api/detail.json')
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      })
         .then(this.getDetailSucc)
     },
     getDetailSucc (res) {
